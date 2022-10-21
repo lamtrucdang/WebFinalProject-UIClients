7fcd15a70f7ceeb3b549fc02a59c41108d3599f3
Lam Truc Dang <19521736@gm.uit.edu.vn>
2022-10-21 15:50:55 +0700

    convert html to react

---
+++ login-page/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Signin from './UI/Signin';
+
+function App() {
+  return (
+    <div>
+      <Signin />
+    </div>
+  );
+}
+
+export default App;
---
+++ login-page/src/UI/Signin.jsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import '../index.css';
+
+const root = 
+    ReactDOM.createRoot(document.getElementById('root'));
+
+function Signin() {
+    return (
+        <div>
+            <meta charSet='UTF-8'/>
+            <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+            <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+            <title>Sign in & Sign up Form</title>
+
+            <main>
+                <div className='box'>
+                    <div className='inner-box'>
+                        <div className='forms-wrap'>
+                            <form action="Signin.jsx" autoComplete='off' className='sign-in-form'>
+                                <div className='logo'>
+                                    <img src="./Logo.png" alt="Glow" />
+                                    <h4>Glow</h4>
+                                </div>
+
+                                <div className='heading'>
+                                    <h2>Welcome Back</h2>
+                                    <h6>Not registered yet?</h6>
+                                    <a href="#" className='toggle'>Sign up</a>
+                                </div>
+
+                                <div className='actual-form'>
+                                    <div className='input-wrap'>
+                                        <input 
+                                            type='text'
+                                            minLength={4}
+                                            className='input-field'
+                                            autoComplete='off'
+                                            required
+                                        />
+                                        <label>Name</label>
+                                    </div>
+
+                                    <div className='input-wrap'>
+                                        <input 
+                                            type='password'
+                                            minLength={8}
+                                            className='input-field'
+                                            autoComplete='off'
+                                            required
+                                        />
+                                        <label>Password</label>
+                                    </div>
+
+                                    {/* <input type="submit" defaultValue={Sign In} className='sign-btn' /> */}
+
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
+}
+
+export default Signin;
