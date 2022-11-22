5807c7bf50c90f00bc677ef52d6f21ff9c38e82a
Le Ngoc Thuy An <19521176@gm.uit.edu.vn>
2022-11-22 23:59:59 +0700

    5th commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,13 +3,15 @@ import TopBar from "./components/topbar/TopBar";
 import Single from "./page/single/Single"
 import Write from "./page/write/Write"
 import Settings from "./page/settings/Settings"
+import Login from "./page/login/Login"
 function App() {
   return (
     <div className="App">
       <TopBar/>
       {/* <Single/> */}
       {/* <Write/> */}
-      <Settings/>
+      {/* <Settings/> */}
+      <Login/>
       <p></p>
     </div>
   );
